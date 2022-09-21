de86b2b5d775711c1adf38f1fe43d67f8168d270
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-09-21 11:15:54 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
