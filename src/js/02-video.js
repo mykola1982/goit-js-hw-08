5193396acfd5fd4e74d411c0c714361f2227ce39
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-09-21 16:17:38 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeupdate = function (data) {
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 250));
+
+const savedData = localStorage.getItem(CURRENT_TIME_KEY);
+const parsedData = JSON.parse(savedData);
+
+if (savedData) {
+  player
+    .setCurrentTime(parsedData.seconds)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
