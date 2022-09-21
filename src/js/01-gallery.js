f846b3160457fb46123af54c25282d1203e92c22
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-09-21 11:32:09 +0300

    fix task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,19 +13,17 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-      <li class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}" 
             alt="${description}" />
-        </a>
-      </li>`;
+        </a>`;
     })
     .join('');
 }
 import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 var lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
